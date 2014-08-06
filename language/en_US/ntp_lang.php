6a69a58535ddad1fec133601832854b11cc57760
clearostranslations <translations@clearfoundation.com>
2014-08-06 16:17:57 -0400

    Prepped for translation server import

--- language/en_US/ntp_lang.php
+++ language/en_US/ntp_lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$lang['ntp_app_name'] = 'NTP Server';
 $lang['ntp_app_description'] = 'The NTP Server app provides the network time protocol service for your systems.  Computers and Internet devices can synchronize their clocks against this server to achieve a  high degree of time accuracy.';
+$lang['ntp_app_name'] = 'NTP Server';
 $lang['ntp_time_servers'] = 'Time Servers';
 $lang['ntp_time_server_thank_you'] = 'Special thanks to ClearCenter for providing time servers.';
