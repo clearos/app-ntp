53bca28437b6465e10551fc5a00a6902b33a0b3f
pcbaldwin <pbaldwin@clearfoundation.com>
2015-10-14 09:41:48 -0400

    Fixed chronyd conflict [tracker #5601]

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'ntp';
-$app['version'] = '2.1.6';
+$app['version'] = '2.1.25';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-ntp.spec
+++ packaging/app-ntp.spec
@@ -1,7 +1,7 @@
 
 Name: app-ntp
 Epoch: 1
-Version: 2.1.6
+Version: 2.1.25
 Release: 1%{dist}
 Summary: NTP Server
 License: GPLv3
