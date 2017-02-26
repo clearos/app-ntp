e9384d0f1fd89c6e83daeb3eaa217cd2536c9438
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:22:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'NTP Server Ilova tizimlari uchun tarmoq vaqt protokol xizmati beradi. Kompyuterlar va Internet qurilmalar vaqt aniqligi yuqori darajasiga erishish uchun bu server qarshi rubrikasini mumkin.';
+$lang['ntp_app_name'] = 'NTP Server';
+$lang['ntp_time_server_thank_you'] = 'vaqt serverlarini ta&#39;minlash uchun ClearCenter alohida rahmat.';
+$lang['ntp_time_servers'] = 'Time Serverlar';
