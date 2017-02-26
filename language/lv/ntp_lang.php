6616540122a7a48460adfa1e5c2e6b20d2333357
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:17:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'NTP Server app nodrošina tīkla laika protokols pakalpojumu jūsu sistēmas. Datori un interneta ierīces var sinhronizēt savus pulksteņus pret šo serveri, lai sasniegtu augstu laika precizitāti.';
+$lang['ntp_app_name'] = 'NTP Server';
+$lang['ntp_time_server_thank_you'] = 'Īpašs paldies ClearCenter lai sniegtu laika serveri.';
+$lang['ntp_time_servers'] = 'laika serveri';
