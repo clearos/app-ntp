70fc34378dd57e0ff5d4f37101ff16ba415db782
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:18:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'Il-app Server NTP jipprovdi s-servizz protokoll ħin tan-netwerk għas-sistemi tiegħek. Kompjuters u apparat Internet jistgħu synchronize arloġġi tagħhom kontra dan is-server biex jinkiseb livell għoli ta &#39;preċiżjoni żmien.';
+$lang['ntp_app_name'] = 'NTP Server';
+$lang['ntp_time_server_thank_you'] = 'Speċjali grazzi għall ClearCenter biex tingħata ħin servers.';
+$lang['ntp_time_servers'] = 'Servers Ħin';
