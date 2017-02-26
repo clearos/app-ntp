5556ee58979d43d13f3a4edfbcc29fcd5e29a1ed
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:14:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'NTP-palvelin-sovellus tarjoaa aikaprotokollalla huoltoa järjestelmiä. Tietokoneet ja Internet laitteet voivat synkronoida kellonsa vastaan ​​tämän palvelimen saavuttaa korkea ajan tarkkuus.';
+$lang['ntp_app_name'] = 'NTP-palvelin';
+$lang['ntp_time_server_thank_you'] = 'Erityiset kiitokset ClearCenter tarjota aikaa palvelimet.';
+$lang['ntp_time_servers'] = 'aikapalvelimia';
