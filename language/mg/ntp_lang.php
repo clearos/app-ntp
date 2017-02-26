f04dbf487be438f9d5569e2503767d993fdaf64f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:17:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'Ny NTP fampiharana Server dia manome fotoana ny tambajotra Protocol fanompoana ho an&#39;ny rafitra. Computers sy ny fitaovana Internet dia afaka synchronize ny famantaranandro loza ho amin&#39;ity lohamilina ny fanatratrarana ny ambony ny fotoana marina.';
+$lang['ntp_app_name'] = 'NTP Server';
+$lang['ntp_time_server_thank_you'] = 'Special noho ny ClearCenter noho ny nanomezany ny fotoana lohamilina.';
+$lang['ntp_time_servers'] = 'Time Servers';
