d4e51e34fe40aaaa32bceb0de082511aa8c182f3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:14:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'NTP સર્વર એપ્લિકેશન તમારી સિસ્ટમો માટે Network Time Protocol સેવા પૂરી પાડે છે. કમ્પ્યુટર્સ અને ઇન્ટરનેટ ઉપકરણો સમય ચોકસાઈ એક ઉચ્ચ ડિગ્રી હાંસલ કરવા માટે આ સર્વર સામે તેમના ઘડિયાળો સુમેળ કરી શકો છો.';
+$lang['ntp_app_name'] = 'NTP સર્વર';
+$lang['ntp_time_server_thank_you'] = 'સમય સર્વરો પૂરી પાડવા માટે ClearCenter ખાસ આભાર.';
+$lang['ntp_time_servers'] = 'સમય સર્વરો';
