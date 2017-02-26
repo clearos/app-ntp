ffc898565027cece6dd1ec3e4333db17535b5202
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:13:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'NTP服务器的应用程序可为您的系统网络时间协议服务。 计算机和互联网设备可以针对此服务器同步的时钟，实现了高度的时间的准确性。';
+$lang['ntp_app_name'] = 'NTP服务器';
+$lang['ntp_time_server_thank_you'] = '特别感谢ClearCenter用于提供时间的服务器。';
+$lang['ntp_time_servers'] = '时间服务器';
