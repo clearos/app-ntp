d57b4088b10c85b3106a22dc191b742315dce1b4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:20:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'The app Server NTP bixisaa adeegga maamuuska waqti network nidaamyada aad. Computers iyo aaladaha Internet waafajinta karaa saacadaha ka dhanka ah server this si ay u gaaraan heerka sare ah sax waqtiga.';
+$lang['ntp_app_name'] = 'Server NTP';
+$lang['ntp_time_server_thank_you'] = 'Mahad gaar ah si ay u ClearCenter bixinta server waqti.';
+$lang['ntp_time_servers'] = 'server Time';
