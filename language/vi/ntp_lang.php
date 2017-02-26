a2b7e4cba0c96fe9d794f5fd1320745ce2c2cb9c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:22:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'Các ứng dụng NTP Server cung cấp dịch vụ giao thức thời gian mạng cho hệ thống của bạn. Máy vi tính và các thiết bị Internet có thể đồng bộ hóa đồng hồ của họ đối với máy chủ này để đạt được một mức độ cao của tính chính xác thời gian.';
+$lang['ntp_app_name'] = 'NTP server';
+$lang['ntp_time_server_thank_you'] = 'Đặc biệt cảm ơn ClearCenter cho việc cung cấp các máy chủ thời gian.';
+$lang['ntp_time_servers'] = 'Máy chủ thời gian';
