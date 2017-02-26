c1416e6ad223263a445885df381a2ccbf99f004a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:12:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'NTP zerbitzari aplikazio sare denbora protokoloa zerbitzua Zure sistemak eskaintzen. Ordenagailuak eta Internet gailu beren erlojuak sinkronizatu ahal zerbitzari honen aurka denbora zehaztasun-maila handia lortzea.';
+$lang['ntp_app_name'] = 'NTP Server';
+$lang['ntp_time_server_thank_you'] = 'ClearCenter esker bereziak denbora zerbitzariak eskainiz.';
+$lang['ntp_time_servers'] = 'Denbora Servers';
