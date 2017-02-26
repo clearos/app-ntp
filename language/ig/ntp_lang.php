3630e4f0a0c5f3d7b729bd13da8ff9193ec47234
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:15:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'The NTP Server ngwa na-enye netwọk oge protocol ọrụ n&#39;ihi na gị usoro. Computers na Internet ngwaọrụ nwere ike ijikọ ha clocks megide a na ihe nkesa iji nweta a elu n&#39;ókè nke oge ziri ezi.';
+$lang['ntp_app_name'] = 'NTP Server';
+$lang['ntp_time_server_thank_you'] = 'Special ekele ClearCenter maka inye oge sava.';
+$lang['ntp_time_servers'] = 'Time Servers';
