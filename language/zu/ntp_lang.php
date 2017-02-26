b800acbcbfd3d70c60e94e26e65ea97778e48ee2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:23:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'Uhlelo NTP Server lokusebenza inikeza isikhathi inethiwekhi protocol service izinhlelo zakho. Ezamakhompiyutha kanye namadivayisi Inthanethi Ungavumelanisa amawashi abo ukuze balwe kule seva ukuze sifinyelele izinga eliphakeme isikhathi nokunemba.';
+$lang['ntp_app_name'] = 'NTP Server';
+$lang['ntp_time_server_thank_you'] = 'Sibonga ngokukhethekile ku-ClearCenter ngokusinika amaseva isikhathi.';
+$lang['ntp_time_servers'] = 'Isikhathi Servers';
