eb470078d0e259ad2538d3fae2d8888a8d55760f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:17:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'NTP serverio programa suteikia Network Time Protocol paslauga jūsų sistemos. Kompiuteriai ir internetas prietaisai gali sinchronizuoti savo laikrodžius su šio serverio pasiekti aukštą laiko tikslumą.';
+$lang['ntp_app_name'] = 'NTP-serverio';
+$lang['ntp_time_server_thank_you'] = 'Specialūs dėka ClearCenter teikimo laiko serverius.';
+$lang['ntp_time_servers'] = 'laiko serveriai';
