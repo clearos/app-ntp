41f9f499dc1d45e1fa0ba5f0d7d2cac309effa68
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:13:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'The app NTP Server amapereka maukonde nthawi protocol utumiki kachitidwe ako. Makompyuta ndi zipangizo Intaneti synchronize mawotchi awo motsutsana makina izi kukwaniritsa digiri yapamwamba ya zolondola nthawi.';
+$lang['ntp_app_name'] = 'NTP Server';
+$lang['ntp_time_server_thank_you'] = 'zikomo Special kuti ClearCenter potipatsa maseva nthawi.';
+$lang['ntp_time_servers'] = 'Time Zatsabola';
