cb3118604e269ea50aa8b0f8a8d96543a4259ab9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:21:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'programu NTP Server hutoa mtandao wakati itifaki huduma kwa mifumo yako. Kompyuta na vifaa Internet inaweza synchronize saa yao dhidi ya seva hii kufikia kiwango cha juu cha usahihi wakati.';
+$lang['ntp_app_name'] = 'NTP Server';
+$lang['ntp_time_server_thank_you'] = 'Shukrani za pekee kwa ClearCenter kwa ajili ya kutoa servrar wakati.';
+$lang['ntp_time_servers'] = 'muda Servers';
