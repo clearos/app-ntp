605b4bbca66bfa795d75b4ccf4d06e96f838b047
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:14:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'L&#39;application de serveur NTP fournit le service de protocole de temps réseau pour vos systèmes. Ordinateurs et appareils compatibles avec Internet peuvent synchroniser leurs horloges sur ce serveur pour atteindre un haut degré de précision du temps.';
+$lang['ntp_app_name'] = 'serveur NTP';
+$lang['ntp_time_server_thank_you'] = 'Remerciements particuliers à ClearCenter pour fournir des serveurs de temps.';
+$lang['ntp_time_servers'] = 'Temps Serveurs';
