430a5bf83588cb1587d9cb685eadf8e233a5c981
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:17:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'Servo in NTP App network praebet ministerium ad tempus protocollo ratio. Cogitationes eorum horologiorum ad Internet potest Home synchronise eminentia tempus ornare velit sagaciter.';
+$lang['ntp_app_name'] = 'NTP Server';
+$lang['ntp_time_server_thank_you'] = 'Praecipua gratias ad ClearCenter modo pro tempore servientibus.';
+$lang['ntp_time_servers'] = 'Tempus Servers';
