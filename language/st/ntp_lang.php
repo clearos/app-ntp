a7714614d0cd44031d32b25256c26aaf816ba45e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:20:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'The app e NTP Server e fana ka ya netweke nako melaoana tšebeletso bakeng sa litsamaiso tsa hao. Lik&#39;homphieutha le lisebelisoa tsa Internet ka nyalanya lioache tsa bona khahlanong le seva ena ho fihlela ka tsela e itseng e phahameng ea ho nepahala ha eona nako.';
+$lang['ntp_app_name'] = 'NTP Server';
+$lang['ntp_time_server_thank_you'] = 'leboha ka ho khethehileng bakeng sa ho fana ka ClearCenter server nako.';
+$lang['ntp_time_servers'] = 'Time server';
