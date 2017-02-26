faaaa7863abf6ff228ddeb3e0c8f577833e0b053
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:16:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'NTP 서버 응용 프로그램은 시스템의 네트워크 시간 프로토콜 서비스를 제공합니다. 컴퓨터와 인터넷 디바이스는 시간 정밀도 높은 수준을 달성하기 위해,이 서버에 대하여 자신의 클록을 동기화 할 수있다.';
+$lang['ntp_app_name'] = 'NTP 서버';
+$lang['ntp_time_server_thank_you'] = '시간 서버를 제공 ClearCenter 특별 감사.';
+$lang['ntp_time_servers'] = '시간 서버';
