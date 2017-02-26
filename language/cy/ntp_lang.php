88ed8a8bd51fbad4b87a79974d3d1127d55389e8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:22:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'Mae&#39;r app Gweinyddwr NTP darparu&#39;r gwasanaeth protocol amser rhwydwaith ar gyfer eich systemau. Gall cyfrifiaduron a dyfeisiau Rhyngrwyd synchronize eu clociau yn erbyn y gweinydd hwn i gyflawni lefel uchel o gywirdeb amser.';
+$lang['ntp_app_name'] = 'NTP Gweinydd';
+$lang['ntp_time_server_thank_you'] = 'Diolch yn arbennig i ClearCenter am ddarparu gweinyddion amser.';
+$lang['ntp_time_servers'] = 'Gweinyddion amser';
