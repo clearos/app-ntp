ada524d684aaeb2b37a7d357ef66de7d399a288d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:17:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'whakarato te taupānga Tūmau NTP te ratonga kawa wā whatunga mo koutou pūnaha. Rorohiko, me ngā pūrere Ipurangi taea te tukutahi ratou karaka ki tenei tūmau ki te whakatutuki i te tohu tiketike o te wā tika.';
+$lang['ntp_app_name'] = 'Tūmau NTP';
+$lang['ntp_time_server_thank_you'] = 'whakawhetai motuhake ki ClearCenter mō te whakarato tūmau wa.';
+$lang['ntp_time_servers'] = 'Tūmau wā';
