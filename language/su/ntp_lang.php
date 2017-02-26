865880c60f79efbda1cba996c293e9879c81fe73
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:21:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'Nu NTP Server app nyadiakeun ladenan protokol waktu jaringan pikeun sistim Anjeun. Komputer jeung alat Internet bisa nyingkronkeun jam maranéhanana ngalawan server ieu ngahontal gelar luhur akurasi waktu.';
+$lang['ntp_app_name'] = 'NTP Server';
+$lang['ntp_time_server_thank_you'] = 'hatur nuhun husus ka ClearCenter pikeun nyadiakeun server waktu.';
+$lang['ntp_time_servers'] = 'server Time';
