af2cff5c0d6a1cad703010e98cd12368726231d1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:16:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'NTPサーバーのアプリは、あなたのシステムのためのネットワーク・タイム・プロトコルサービスを提供しています。 コンピュータとインターネットデバイスは、時間精度の高い学位を達成するために、このサーバーに対してそれらのクロックを同期させることができます。';
+$lang['ntp_app_name'] = 'NTPサーバー';
+$lang['ntp_time_server_thank_you'] = 'タイムサーバを提供するためのClearCenterに感謝します。';
+$lang['ntp_time_servers'] = 'タイムサーバ';
