fbbcd623100b6fc5532172144b042e19e5b81982
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:13:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'La NTP servilo app provizas la reto tempo protokolo servo por viaj sistemoj. Komputiloj kaj Interreto mekanismoj povas sinkronigi liajn horloĝojn kontraŭ tiu servilo atingi altan gradon de tempo precizeco.';
+$lang['ntp_app_name'] = 'NTP Servilo';
+$lang['ntp_time_server_thank_you'] = 'Specialaj danke al ClearCenter por disponigado tempo serviloj.';
+$lang['ntp_time_servers'] = 'tempo Serviloj';
