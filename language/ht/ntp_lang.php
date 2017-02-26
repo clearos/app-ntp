1a35113326ad697fbe1268b2d6d207032314e20e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:15:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'Sèvè App a np bay sèvis la pwotokòl tan rezo pou sistèm ou yo. Odinatè ak aparèy entènèt ka senkroniz revèy yo kont sa a sèvè reyalize yon wo degre de presizyon tan.';
+$lang['ntp_app_name'] = 'np sèvè';
+$lang['ntp_time_server_thank_you'] = 'Mèsi espesyal ClearCenter pou bay serveurs tan.';
+$lang['ntp_time_servers'] = 'Servers Tan';
