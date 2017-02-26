f5372a74b3af86ea94cb14cc5db7453e5b8a7000
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:20:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'Server aplikacija NTP zagotavlja protokol storitev časovnega omrežja za vaš sistem. Računalništvo in internet naprave lahko sinhronizirate svoje ure pred tem strežniku, da bi dosegli visoko stopnjo natančnosti časa.';
+$lang['ntp_app_name'] = 'NTP Server';
+$lang['ntp_time_server_thank_you'] = 'Posebna zahvala ClearCenter za zagotavljanje časovnih strežnikov.';
+$lang['ntp_time_servers'] = 'časovni strežniki';
