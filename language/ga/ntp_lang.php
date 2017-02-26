062552ae344bb8b209463c6da6d1a90c88d7afd8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 19:15:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/ntp_lang.php
@@ -0,0 +1,6 @@
+<?php
+
+$lang['ntp_app_description'] = 'Soláthraíonn an app Freastalaí NTP an tseirbhís phrótacail am líonra do do chórais. Is féidir le ríomhairí agus gléasanna Idirlín shioncrónú gcuid cloig in aghaidh an freastalaí a bhaint amach ar leibhéal ard cruinnis ama.';
+$lang['ntp_app_name'] = 'NTP Freastalaí';
+$lang['ntp_time_server_thank_you'] = 'Buíochas speisialta le ClearCenter as freastalaithe ama.';
+$lang['ntp_time_servers'] = 'Freastalaithe Am';
